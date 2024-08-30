0bde161c69eb563d9cc4b7a6437a4d5e5853ff97
ilima12091 <limaignacio12901@gmail.com>
2024-08-30 18:24:50 -0300

    Se agregan mejoras a la pantalla de login

--- ProyectoInicial/app/src/main/java/com/example/proyectoinicial/MainActivity.kt
+++ ProyectoInicial/app/src/main/java/com/example/proyectoinicial/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.proyectoinicial
 
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,16 +12,25 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,25 +54,84 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(16.dp)) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
         LoginLogo(modifier = Modifier.fillMaxWidth())
-        OutlinedTextField(modifier = Modifier.fillMaxWidth(), value = "", onValueChange = {}, label = { Text(text = "Email input")})
-        OutlinedTextField(modifier = Modifier.fillMaxWidth(), value = "", onValueChange = {}, label = {Text(text = "Password input")})
-        Text(text = "Forgot Password?", modifier = Modifier.align(Alignment.End))
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(text = "Email input") })
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password input") },
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image = if (passwordVisible)
+                    painterResource(id = R.drawable.ic_visibility)
+                else
+                    painterResource(id = R.drawable.ic_visibility_off)
+
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(
+                        painter = image,
+                        contentDescription = if (passwordVisible) "Hide password" else "Show password"
+                    )
+                }
+            }
+        )
+        TextButton(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.End)) {
+            Text(text = "Forgot Password?")
+        }
+        Button(onClick = {
+            email = ""
+            password = ""
+        }, modifier = Modifier.fillMaxWidth()) {
             Text(text = "LOGIN")
         }
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Text(text = "Don't have an account?")
-            Text(text = "Sign-up")
-        }
-        Text(text = "Or login with", textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally)) {
-            Button(onClick = { /*TODO*/ } ) {
-                Icon(painter = painterResource(id = R.drawable.icon_gmail), contentDescription = "Gmail logo")
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Sign-up")
             }
-            Button(onClick = { /*TODO*/ }) {
-                Icon(painter = painterResource(id = R.drawable.icon_facebook), contentDescription = "Facebook logo")
+        }
+        Text(
+            text = "Or login with",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally)
+        ) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Image(
+                    painter = painterResource(id = R.drawable.icon_gmail),
+                    contentDescription = "Gmail icon",
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.icon_facebook
+                    ),
+                    contentDescription = "Facebook logo",
+                    modifier = Modifier.size(32.dp)
+                )
             }
         }
     }
@@ -73,8 +139,15 @@ fun LoginScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun LoginLogo(modifier: Modifier = Modifier) {
-    Box(modifier = modifier, ) {
-        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "Login logo",  modifier = Modifier.align(Alignment.Center))
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Login logo",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillBounds
+        )
         Text(text = "Log in", modifier = Modifier.align(Alignment.Center))
     }
 }
